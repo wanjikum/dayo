32f2c854c32043b3268048ea496a3bbe329f259d
wanjikum <wanjikumillicent5@gmail.com>
2017-02-06 16:34:00 +0300

    Failing tests for negative, zero and one inputs

--- testprime.py
+++ testprime.py
@@ -12,12 +12,27 @@ class TestPrime(unittest.TestCase):
     def test_if_it_accepts_dictionary(self):
         """Test if dictionary datatype is accepted"""
         with self.assertRaises(TypeError):
-            primenum("{}")
+            primenum({})
 
     def test_if_it_accepts_lists(self):
         """Test if list datatype is accepted"""
         with self.assertRaises(TypeError):
-            primenum("[]")
+            primenum([])
+
+    def test_if_input_is_negative(self):
+    	"""Tests if it accepts negative numbers"""
+    	self.assertEquals(primenum(-5), "Numbers less than or equal to zero are not allowed!")
+
+    def test_if_input_is_one(self):
+    	"""Tests if one is a prime number"""
+    	self.assertEquals(primenum(1), "One is not considered prime number")
+
+    def test_if_input_is_zero(self):
+    	"""Tests if zero is a prime number"""
+    	self.assertEquals(primenum(0), "Numbers less than or equal to zero are not allowed!")
+
+    
+
 
 
 
