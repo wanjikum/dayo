9eafc933a3e0aed27a64f4630781b97fcf7f9279
wanjikum <wanjikumillicent5@gmail.com>
2017-02-06 16:13:49 +0300

    Failing tests for datatype values

--- testprime.py
+++ testprime.py
@@ -1,2 +1,32 @@
+"""A collection of test cases that checks if the program is working well"""
 import unittest
 from prime import primenum
+
+class TestPrime(unittest.TestCase):
+    """A class that contains testcases of primenumbers"""
+    def test_if_it_accepts_string_datatype(self):
+        """Tests if string datatype is accepted"""
+        with self.assertRaises(TypeError):
+            primenum("string")
+
+    def test_if_it_accepts_dictionary(self):
+        """Test if dictionary datatype is accepted"""
+        with self.assertRaises(TypeError):
+            primenum("{}")
+
+    def test_if_it_accepts_lists(self):
+        """Test if list datatype is accepted"""
+        with self.assertRaises(TypeError):
+            primenum("[]")
+
+
+
+
+
+
+
+    
+    
+
+
+
