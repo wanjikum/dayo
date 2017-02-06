837def7d74f001a3ff1034963f4607087b2ab2f0
wanjikum <wanjikumillicent5@gmail.com>
2017-02-06 17:15:51 +0300

    Failing tests in the functionality of the code

--- prime.py
+++ prime.py
@@ -4,3 +4,10 @@ def primenum(number):
 	"""A function that generates prime numbers in n"""
 	if not isinstance(number, int):
 		raise TypeError
+
+	if number <= 0:
+		return "Numbers less than or equal to zero are not allowed!"
+
+	elif number == 1:
+		return "One is not considered prime number"
+		
--- testprime.py
+++ testprime.py
@@ -19,6 +19,11 @@ class TestPrime(unittest.TestCase):
         with self.assertRaises(TypeError):
             primenum([])
 
+    def test_if_it_accepts_lists(self):
+        """Test if list datatype is accepted"""
+        with self.assertRaises(TypeError):
+            primenum(56.58)
+
     def test_if_input_is_negative(self):
     	"""Tests if it accepts negative numbers"""
     	self.assertEquals(primenum(-5), "Numbers less than or equal to zero are not allowed!")
@@ -31,7 +36,31 @@ class TestPrime(unittest.TestCase):
     	"""Tests if zero is a prime number"""
     	self.assertEquals(primenum(0), "Numbers less than or equal to zero are not allowed!")
 
+    def test_if_it_outputs_correct_output(self):
+    	"""Tests if it outputs correct result"""
+    	self.assertEquals(primenum(5), [2, 3, 5])
+
+    def test_if_it_outputs_correct_output_for_numbers_greater_than_50(self):
+    	"""Tests if it outputs correct result"""
+    	self.assertEquals(len(primenum(55)), 16)
+
+    def test_if_it_includes_a_number_i(self):
+    	"""Tests if a number is prime is included in the list"""
+    	self.assertIn(5, primenum(5))
+
+    def test_if_it_includes_a_number_if_the_number_is(self):
+    	"""Tests the input number is prime is included in the list"""
+    	self.assertNotIn(16, primenum(16))
+
+
+  
+
+
+
     
+
+
+
 
 
 
