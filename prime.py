28edf5dc64ac517ed59a8964802c710998bade15
wanjikum <wanjikumillicent5@gmail.com>
2017-02-06 18:24:11 +0300

    All the tests pass

--- prime.py
+++ prime.py
@@ -2,6 +2,7 @@
 
 def primenum(number):
 	"""A function that generates prime numbers in n"""
+	result = []
 	if not isinstance(number, int):
 		raise TypeError
 
@@ -10,4 +11,13 @@ def primenum(number):
 
 	elif number == 1:
 		return "One is not considered prime number"
-		
+
+	else:
+		for i in range(2, number+1):
+			isprime = True
+			for j in range(2, i):
+				if i%j == 0:
+					isprime = False
+			if isprime:
+				result.append(i)
+	return result
